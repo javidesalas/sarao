5d10efad8c808471a33268a2d526f28b92a6b440
Javier de Salas <chinostrike@gmail.com>
2020-09-20 18:30:32 +0200

    Event creation complete with population

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -3,23 +3,26 @@ const router = express.Router()
 
 const User = require('../models/user.model')
 const Event = require('../models/event.model')
-
+const Sarao = require('../models/sarao.model')
 
 router.get('/new', (req, res) => {
+    const activeSarao = req.user.activeSarao
+    const activeUser = req.user //paso el usuario activo y el sarao activo al renderizado
 
-    res.render('event/new-event')
-    const {activeSarao} = req.user
-
+    console.log (activeSarao)
     Sarao.findById(activeSarao)
         .populate('userList')
-        .then(activeSarao => res.render('event/new-event', activeSarao ))
+        .then(activeSarao => res.render('event/new-event', {activeSarao, activeUser} ))
         .catch(err => console.log ('Waddaflurb Morty!!', err))
 
 })
 
 router.post('/new', (req, res) => {
-    const {name, startDate} = req.body
-    Event.create({name, startDate})
+    const {name, image, description, startDate, duration, location, karmaPlus, karmaMinus, userPlus, userMinus} = req.body
+    const owner = req.query.owner
+    const sarao = req.query.sarao
+
+    Event.create({name, owner, sarao, image, description, startDate, duration, location, karmaPlus, karmaMinus, userPlus, userMinus})
         .then(() => res.redirect('/'))
         .catch(err => console.log ('Waddaflurb Morty!!', err))
 
