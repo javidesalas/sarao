bbc66b4cfe141e7f3067756215c86c1771fb0b54
Sergio <sergiobaltanasmontes@gmail.com>
2020-09-21 20:05:45 +0200

    login condition

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const User = require("../models/user.model")
+const isLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Desautorizado, inicia sesión para continuar' })
 
 router.get('/', (req, res) => res.render('index'))
-router.get('/ranking', (req, res) => res.render('ranking'))
+router.get('/ranking', isLoggedIn, (req, res, next) => {
+
+
+    User.find()
+        .then(karmaUser => res.render('ranking', { karmaUser }))
+        .catch(err => next(err))
+})
 
 module.exports = router
