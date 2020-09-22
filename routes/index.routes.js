51c3b535f698f8a07bb9792e0acc722bcf3d5049
Javier de Salas <chinostrike@gmail.com>
2020-09-22 12:00:32 +0200

    index working

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const User = require("../models/user.model")
+const Event = require("../models/event.model")
 const isLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Desautorizado, inicia sesión para continuar' })
 const actUser = require('../configs/userLocals.config')
 
 
-router.get('/', actUser, (req, res) => res.render('index'))
-router.get('/ranking', actUser, isLoggedIn, (req, res, next) => {
+router.get('/', isLoggedIn,actUser, (req, res) => {
+    const {activeSarao} = req.user
+    Event.find({sarao: activeSarao}).sort({startDate : -1})
+        .populate('sarao')
+        .then(events => res.render('index', {events}))
+        .catch(err => next(err))
+})
 
 
+router.get('/ranking', isLoggedIn, actUser,  (req, res, next) => {
     User.find().sort({ karma: -1 })
         .then(karmaUser => res.render('ranking', { karmaUser }))
         .catch(err => next(err))
