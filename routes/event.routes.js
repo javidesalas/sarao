7cff70922075d9519d8a414f6d5071e0076027a6
Javier de Salas <chinostrike@gmail.com>
2020-09-23 21:14:02 +0200

    fusion

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -96,21 +96,6 @@ router.get('/delete/:id', (req, res) => {
 router.post('/close/:id', isLoggedIn, (req, res, next) => {
     const eventId = req.params.id
 
-<<<<<<< HEAD
-    function buildQueryString(array) {
-        let queryString = `[`
-        array.forEach(elm => {
-            queryString += `{"_id": "`
-            queryString += elm
-            queryString += `"}, `
-        })
-        queryString = queryString.slice(0, -2)
-        queryString += "]"
-        return JSON.parse(queryString)
-    }
-
-    function splitKarma(listArray, totalKarma) {
-=======
     Event.findByIdAndUpdate(eventId, {finished : true})
     .then(closedEvent => {
         Promise.all([splitKarma(closedEvent.userPlus, closedEvent.karmaPlus), (splitKarma(closedEvent.userMinus, closedEvent.karmaMinus))])
@@ -123,96 +108,10 @@ router.post('/close/:id', isLoggedIn, (req, res, next) => {
     .catch(err => console.log(err))
     
     function splitKarma (listArray, totalKarma) {
->>>>>>> master
         const splitKarmaPerUser = totalKarma / listArray.length
-        let queryString = buildQueryString(listArray)
-
+        let queryString = buildQueryString (listArray)
+    
         User.find().or(queryString)
-<<<<<<< HEAD
-            .then(userArray => {
-                return User.updateMany({ _id: { $in: userArray } }, { $inc: { karma: splitKarmaPerUser } })
-            })
-            .then(elm => console.log(elm))
-            .catch(err => console.log('ERRRRRROOOOOR', err))
-
-
-    }
-
-    Event.findByIdAndUpdate(eventId, { finished: true })
-        .then(closedEvent => {
-            Promise.all([splitKarma(closedEvent.userPlus, closedEvent.karmaPlus), (splitKarma(closedEvent.userMinus, closedEvent.karmaMinus))])
-                .then(elm => {
-                    console.log(elm)
-                    res.redirect('/')
-                })
-                .catch(err => console.log('&&&&&&&&&&&&&&&&&&', err))
-        })
-        .catch(err => console.log('ERRRRRROOOOOR', err))
-
-
-    // Retorna un evento con un array de ids userPlus y otro userMinus
-    //         const kPlusPerUser = closedEvent.karmaPlus / closedEvent.userPlus.length
-    //         const kMinusPerUser = closedEvent.karmaMinus / closedEvent.userMinus.length
-    //         console.log(kPlusPerUser)
-    // // Iteramos el array de ids y creamos la query
-    //         let queryPlusString = buildQueryString (closedEvent.userPlus)
-
-    // // Hacemos un findById y nos devuelve un array de objetos con Id y Karma
-    //         User.find().or(queryPlusString)
-    //             .then(elmo =>  User.updateMany ( { _id: { $in: elmo} }, { $inc: { karma: kPlusPerUser } })) 
-    //                                 .then (elm => console.log(elm))   
-    //                                 .catch (err => console.log('!!!!!!!!!!!!!', err))  
-
-
-    //             .catch(err => console.log('&&&&&&&&&&&&&&&&&&', err))
-    //     })        
-
-    // Hacemos un updateMany con $inc + karma
-
-
-})
-
-
-
-
-
-
-
-//     closedEvent.userPlus.forEach(elm => {
-//         const userId = elm
-//         let newUserKarma = kPlusPerUser
-//         User.findById(userId)
-//             .then(userToEdit => {
-//                 console.log('PAQUITAAAAAAAAAAAAAAA',userId, userToEdit)
-//                 newUserKarma += userToEdit.karma
-//                 User.findByIdAndUpdate(userId, {karma: newUserKarma})
-//             })
-//             .then()
-//             .catch(err => console.log('Waddaflurb Morty!!es el update de user Karmas++', err))
-//     });
-
-//     closedEvent.userMinus.forEach(elm => {
-//         const userId = elm
-//         let newUserKarma = kMinusPerUser
-//         User.findById(userId)
-//             .then(userToEdit => {
-//                 console.log('PAQUITOOOOOOOOOOO',userId, userToEdit)
-//                 newUserKarma += userToEdit.karma
-//                 User.findByIdAndUpdate(userId, {karma: newUserKarma})
-//             })
-//             .then()
-//             .catch(err => console.log('Waddaflurb Morty!!es el update de user KarmasMinus', err))
-//     });
-//     res.redirect('/')         
-// })
-// .catch(err => console.log('Waddaflurb Morty!!', err))
-
-
-
-
-
-
-=======
                     .then(userArray => { 
                         return User.updateMany ( { _id: { $in: userArray} }, { $inc: { karma: splitKarmaPerUser } })
                     })
@@ -232,6 +131,5 @@ router.post('/close/:id', isLoggedIn, (req, res, next) => {
     }
 })
 
->>>>>>> master
 module.exports = router
 
