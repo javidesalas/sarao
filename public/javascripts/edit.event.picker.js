a12b1458e8d815b1f986630f4b5e78370d1d69d0
Javier de Salas <chinostrike@gmail.com>
2020-09-24 20:46:09 +0200

    facepicker implemented

---
+++ public/javascripts/edit.event.picker.js
@@ -0,0 +1,15 @@
+
+const minusForm = document.querySelectorAll('.character-minus-form option')
+const minusPicker = document.querySelectorAll('.character-minus-picker .facecard .picframe')
+const minuslist = document.querySelectorAll('.character-minus-list li')
+
+const plusForm = document.querySelectorAll('.character-plus-form option')
+const plusPicker = document.querySelectorAll('.character-plus-picker .facecard .picframe')
+const pluslist = document.querySelectorAll('.character-plus-list li')
+
+graphicSelector (minusForm, minusPicker, minuslist)
+
+graphicSelector (plusForm, plusPicker, plusList)
+
+
+
--- public/javascripts/facepicker.js
+++ public/javascripts/facepicker.js
@@ -1,27 +1,32 @@
 
-const minusForm = document.querySelector('.character-minus-form')
-const minusPicker = document.querySelectorAll('.character-minus-picker .facecard .picframe')
-console.log (minusForm, minusPicker)
-minusPicker.forEach(elm =>{
-    elm.onclick = e => { console.log)
-    }   
-})
-
-// onclick = e =>{
-//     console.log('***********',clickedId)
-//     const clickedId = e.currentTarget.formValue
-//     minusPicker.forEach(elm => {
-//         elm.value === clickedId ? console.log(elm.value) : null
-//     })
-
-
-
-
-// }
-// document.querySelectorAll('p').forEach(elm => {
-
-//     elm.onclick = e => {        // <== objeto event!
-
-//         e.currentTarget.style.color = 'red'
-//     }
-// })
+function graphicSelector (form, picker, list) {
+    if (!list) {list = []}
+    //Take the items from the hidden list and set the corresponding images as activated
+    picker.forEach( pic => {
+        const picId = pic.title
+        list.forEach( li => {
+            li.innerText === picId? pic.classList.toggle('select'): null
+        })
+    })
+    //Mark those ids in the hidden form
+    picker.forEach( pic => {
+        if (pic.classList.contains('select')) {
+            const picId = pic.title
+            form.forEach(elm => {
+                console.log(elm.value, picId)
+                elm.value === picId ? elm.setAttribute('selected','selected') : null
+            })
+        }
+    })
+    //Launch the event so the user can select and deselect clicking the images and it reflects on the form for submission
+    picker.forEach(elm =>{
+        elm.onclick = e => { 
+            e.currentTarget.classList.toggle('select')
+            const picId = e.currentTarget.title
+            console.log(picId)
+            form.forEach(elm => {             
+                (elm.value === picId) ? elm.setAttribute('selected','selected') : null        
+            })                    
+        }   
+    })
+}
