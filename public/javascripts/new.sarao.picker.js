e09ad58b5b7a04916ec6036f61f9fb9767f62c7a
Javier de Salas <chinostrike@gmail.com>
2020-09-25 11:46:59 +0200

    ultimo final definitivo del todo.V3

---
+++ public/javascripts/new.sarao.picker.js
@@ -0,0 +1,12 @@
+
+
+const plusForm = document.querySelectorAll('.character-plus-form option')
+const plusPicker = document.querySelectorAll('.character-plus-picker .facecard .picframe')
+const plusList = []
+
+
+
+graphicSelector (plusForm, plusPicker, plusList)
+
+
+
